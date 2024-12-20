e391a5f4c1a92ff0a1bb247ea7fbfc971ba5c4ca
shimizu-fm <hiroki@freemind.co.jp>
2024-12-20 17:00:32 +0900

    チュートリアル

--- FirstIonideProject/obj/Debug/net9.0/FirstIonideProject.AssemblyInfo.fs
+++ FirstIonideProject/obj/Debug/net9.0/FirstIonideProject.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011d0e323b06ce2381bbb33bca2e29a40a283a03")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b7e1f5f0936c786c320d8de7d19c10356020a27")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
